b52e544223e041a6738ba3afc7328557b73facc2
Anderson Lima <caioanderson022@gmail.com>
2020-04-01 10:24:33 -0300

    Frontend finalizado 2.0

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -48,7 +48,7 @@ export default function Register(){
 
                     <Link  className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
-                            Não tenho cadastro</Link>
+                            Voltar</Link>
         </section>
         
         <form onSubmit={handleRegister}>
